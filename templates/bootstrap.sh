133f824f8830b533fc91af51a30446220d1d6372
srb3 <sbrown@chef.io>
2020-03-14 09:26:05 +0000

    vimrc update

--- templates/bootstrap.sh
+++ templates/bootstrap.sh
@@ -336,6 +336,9 @@ noremap <Left> <NOP>
 noremap <Right> <NOP>
 imap jk <Esc>
 vmap ff <Esc>
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+endif
 EOF
 
 cat << EOF > /home/${user_name}/.tmux.conf
